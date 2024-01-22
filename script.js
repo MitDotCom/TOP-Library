d5eba94c801e148748da35293c38c8311cbbe7ab
Mitchell <Mitchellrcomrie@gmail.com>
2024-01-22 14:25:57 -0800

    Add initial variables

---
+++ script.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+function Book(title,author,pages,read,rating) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.rating = rating;
+}
+
+function addBookToLibrary() {
+    
+}
