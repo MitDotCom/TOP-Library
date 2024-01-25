f257f4e0eec2803257a216d3e5f93e403287aa42
Mitchell <Mitchellrcomrie@gmail.com>
2024-01-24 21:42:10 -0800

    Script 'add book' dialog modal, style in CSS

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function Book(title,author,pages,read,rating,divName) {
     this.pages = pages;
     this.read = read;
     this.rating = rating;
+    // For CSS element selector
     this.divName = divName;
 };
 
@@ -43,12 +44,72 @@ function displayBooks() {
 
 function addBook() {
     let addDialog = document.createElement("dialog");
-    addDialog.classList.add("add");
-    addDialog.textContent = "My name is Lorem"; 
+    addDialog.setAttribute("id","add-modal");
+    addDialog.setAttribute("method","dialog");
+
     let closeButton = document.createElement("button");
     closeButton.textContent = "X";
     closeButton.classList.add("close");
     addDialog.appendChild(closeButton);
+
+    let modalForm = document.createElement("form");
+
+    let modalTitle = document.createElement("input");
+    modalTitle.setAttribute("type","text");
+    modalTitle.setAttribute("id","title");
+    modalTitle.setAttribute("name","title");
+    let modalTitleLabel = document.createElement("label");
+    modalTitleLabel.setAttribute("for","title");
+    modalTitleLabel.textContent = "Title:";
+    modalForm.appendChild(modalTitleLabel);
+    modalForm.appendChild(modalTitle);
+
+    let modalAuthor = document.createElement("input");
+    modalAuthor.setAttribute("type","text");
+    modalAuthor.setAttribute("id","author");
+    modalAuthor.setAttribute("name","author");
+    let modalAuthorLabel = document.createElement("label");
+    modalAuthorLabel.setAttribute("for","author");
+    modalAuthorLabel.textContent = "Author:";
+    modalForm.appendChild(modalAuthorLabel);
+    modalForm.appendChild(modalAuthor);
+
+    let modalPages = document.createElement("input");
+    modalPages.setAttribute("type","text");
+    modalPages.setAttribute("id","pages");
+    modalPages.setAttribute("name","pages");
+    let modalPagesLabel = document.createElement("label");
+    modalPagesLabel.setAttribute("for","pages");
+    modalPagesLabel.textContent = "Pages:";
+    modalForm.appendChild(modalPagesLabel);
+    modalForm.appendChild(modalPages);
+
+    let modalRead = document.createElement("input");
+    modalRead.setAttribute("type","text");
+    modalRead.setAttribute("id","read");
+    modalRead.setAttribute("name","read");
+    let modalReadLabel = document.createElement("label");
+    modalReadLabel.setAttribute("for","read");
+    modalReadLabel.textContent = "Read:";
+    modalForm.appendChild(modalReadLabel);
+    modalForm.appendChild(modalRead);
+
+    let modalRating = document.createElement("input");
+    modalRating.setAttribute("type","text");
+    modalRating.setAttribute("id","rating");
+    modalRating.setAttribute("name","rating");
+    let modalRatingLabel = document.createElement("label");
+    modalRatingLabel.setAttribute("for","rating");
+    modalRatingLabel.textContent = "Rating:";
+    modalForm.appendChild(modalRatingLabel);
+    modalForm.appendChild(modalRating);
+
+    let modalSubmit = document.createElement("button");
+    modalSubmit.setAttribute("type","submit");
+    modalSubmit.textContent = "Submit";
+    modalForm.appendChild(modalSubmit);
+
+    addDialog.appendChild(modalForm);
     document.body.appendChild(addDialog);
 
     addDialog.showModal();
